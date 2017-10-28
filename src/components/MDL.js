a1145e816e17cee019af42d1d681af4a45a0dfcf
Voinia Adrian <voiniaadrian@yahoo.com>
2017-10-28 18:35:57 +0300

    task1ui

--- src/components/MDL.js
+++ src/components/MDL.js
@@ -3,16 +3,20 @@ import { Button, Card, CardText, CardTitle, CardActions, CardMenu, IconButton } 
 
 class ExampleMDL extends Component {
 
+    takePicture(){
+        alert("New picture");
+    }
+
     render() {
         return (
             <Card shadow={0} style={{ width: '320px', height: '320px', margin: 'auto' }}>
-                <CardTitle expand style={{ color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC' }}>Update</CardTitle>
+                <CardTitle expand style={{ color: '#fff', background: 'grey' }}>Task 1: New visitor at the door</CardTitle>
                 <CardText>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Aenan convallis.
+                    You have a new visitor at the door! Do you want to see who is out there?
                 </CardText>
                 <CardActions border>
-                    <Button colored>View Updates</Button>
+                    <Button colored>Ignore</Button>
+                    <Button colored onClick={this.takePicture}>Take picture</Button>
                 </CardActions>
             </Card>
 
